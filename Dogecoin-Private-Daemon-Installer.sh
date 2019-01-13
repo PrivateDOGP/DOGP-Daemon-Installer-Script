f1abd90801568a4a1f231523b272d63759eb71e8
cryptojoe <cryptojoehodler@gmail.com>
2019-01-13 08:38:49 -0500

    Initial commit

---
+++ Dogecoin-Private-Daemon-Installer.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Functions
+add_ppa(){
+  grep -h "bitcoin" /etc/apt/sources.list.d/* > /dev/null
+  if [ $? -ne 0 ]
+  then
+    echo "Adding Bitcoin PPA."
+    yes "" | add-apt-repository ppa:bitcoin/bitcoin
+  else
+    echo "Bitcoin PPA already exists!"
+  fi
+}
+
+# Entrypoint
+cd ..
+
+# Add Bitcoin PPA
+add_ppa && apt-get update
+
+# Install Dependancies
+apt-get -qq install zip curl \
+        build-essential \
+        libtool \
+        autotools-dev \
+        autoconf automake pkg-config \
+        libssl-dev \
+        libevent-dev \
+        bsdmainutils \
+        git cmake \
+        libboost-all-dev \
+        software-properties-common \
+        libdb4.8-dev libdb4.8++-dev \
+        libminiupnpc-dev libzmq3-dev \
+        libssl1.0-dev libgmp3-dev
+
+# Clone the Dogecoin Private Core repository and compile
+git clone https://github.com/PrivateDOGP/DOGP-Project.git
+cd DOGP-Project
+./autogen.sh
+./configure --disable-tests --with-unsupported-ssl --without-gui
+make -j$(nproc)
+echo "Build complete. Installing now!"
+make install
+
+chown -R $(logname): ../DOGP-Project
+
+echo
+echo -e "\e[1m\e[92mSetup complete! Now run /usr/local/bin/dogecoinprivated --daemon\e[0m"
+echo
---
+++ README.md
@@ -0,0 +1,14 @@
+# Dogecoin Private Daemon Installer Script for Ubuntu 18.04 OS
+
+The following script will automate the install of the Dogecoin Private Daemon.
+
+Clone this repository with ```https://github.com/PrivateDOGP/DOGP-Daemon-Installer-Script.git```.
+
+## DOGP-Daemon-Installer-Script
+Use this script to setup the Dogecoin Private daemon. 
+
+Change into the cloned directory ```cd DOGP-Daemon-Installer-Script```
+
+Run the setup script as root using ```sudo ./Dogecoin-Private-Daemon-Installer.sh```
+
+Once the installer is complete, run the daemon with ```/usr/local/bin/dogecoinprivated --daemon```
